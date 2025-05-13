7e244108557de4115003be7de2012d829bdef7f0
alextsyg972 <tsyganovalexandr98@gmail.com>
2025-05-13 20:53:53 +1000

    init

--- Notifications/src/main/java/org/example/notifications/service/NotificationServiceImpl.java
+++ Notifications/src/main/java/org/example/notifications/service/NotificationServiceImpl.java
@@ -3,12 +3,7 @@ package org.example.notifications.service;
 import org.example.core.OrderCreatedEvent;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.CompletableFuture;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
